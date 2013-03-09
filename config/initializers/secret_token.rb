b1540d0e0949863a7fc267ba015dc51e635701d4
Pedro Marce Rodriguez <pedromarce@gmail.com>
2013-03-09 21:56:07 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nubemovie::Application.config.secret_token = 'c81bb1179e894bab326068b5986b6bf3f38985e505e1880d23a3676c8f2f2ee960ff0f85d46b1850009c6f11e9a924a7c59824de779d96a8daceaf181064bd78'
